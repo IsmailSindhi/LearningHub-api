8cdff40fadb38066e3f88fc547a3b0240553f759
ismailsindhi <muhammadismailsoomro09@gmail.com>
2023-01-13 05:22:22 +0500

    sec

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ const CONNECTION_URL = "mongodb+srv://isoomro:isoomro@cluster0.stkwc.mongodb.net
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/faculty", facultyRoutes);
